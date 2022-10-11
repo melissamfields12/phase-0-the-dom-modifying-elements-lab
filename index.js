008187ff473b5bd8e7adbb259a3777fc17f4053d
Melissa Fields <melissamfields12@gmail.com>
2022-10-11 13:19:15 -0700

    commit tree da75dedcc0b82ab13b043bca157d94e36ef0bbbb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let h1 = document.querySelector('h1');
+newHeader.textContent = "Melissa is the champion";
+document.body.append(newHeader);
+
